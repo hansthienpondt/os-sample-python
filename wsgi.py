c1316ccf9cc39549491e53510076ed478a057792
Hans Thienpondt <hans.thienpondt@nokia.com>
2017-11-30 16:05:22 +0100

    Openshift Sample app: display source IP client

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    #return "Hello World!"
+    return jsonify({'ip': request.remote_addr})
 
 if __name__ == "__main__":
     application.run()
