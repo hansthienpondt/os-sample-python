38196535f3bb7973e4db121c8250616c17191194
Hans Thienpondt <hans.thienpondt@nokia.com>
2017-11-30 16:51:35 +0100

    Openshift Sample app: adding client IP, x-forwarded-for

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,7 @@ application = Flask(__name__)
 def hello():
     #return "Hello World!"
     #return jsonify({'ip': request.remote_addr})
-    return "This is an example wsgi app served from {} from proxy {} to client {}".format(socket.gethostname(), request.remote_addr, request.environ['REMOTE_ADDR'])
+    return "This is an example wsgi app served from {} from proxy {} to client {}".format(socket.gethostname(), request.remote_addr, request.headers.get('X-Forwarded-For'))
 
 if __name__ == "__main__":
     application.run()
